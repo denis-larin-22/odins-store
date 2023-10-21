f08896d3dc47042653578428c6ba7bf580b13e95
denis-larin-22 <denislarin2017@gmail.com>
2023-10-21 21:41:25 +0300

    Added .env

--- src/api/firebase/firebase.js
+++ src/api/firebase/firebase.js
@@ -1,10 +1,11 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from '@firebase/firestore';
 import { getStorage } from "firebase/storage";
 
+const key = process.env.REACT_APP_FIREBASE_KEY;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyApwaFlra0EUNbcahWA9xfi086nacj_Kro",
+  // apiKey: key,
   authDomain: "odins-store.firebaseapp.com",
   projectId: "odins-store",
   storageBucket: 'gs://odins-store.appspot.com/',
